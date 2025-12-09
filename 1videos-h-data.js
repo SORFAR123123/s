f945cb2af841ec9896ea115f68c99fe1290a9b5e
SORFAR123123 <nino12171f@gmail.com>
2025-12-09 12:28:21 -0500

    Create 1videos-h-data.js

---
+++ 1videos-h-data.js
@@ -0,0 +1,179 @@
+// ============================================================================
+// BASE DE DATOS DE VOCABULARIO H/ROMÁNTICO - PARA VIDEOS H
+// ============================================================================
+
+const vocabularioH = {
+    // Colección 1 - Hermana Mayor
+    'coleccion1': {
+        'mazo1': [
+            { japones: '愛撫', lectura: 'aibu', opciones: ['Caricias', 'Besos', 'Abrazos', 'Miradas'], respuesta: 0 },
+            { japones: '官能', lectura: 'kannou', opciones: ['Placer sensual', 'Dolor', 'Frío', 'Calor'], respuesta: 0 },
+            { japones: '濡れる', lectura: 'nureru', opciones: ['Mojarse', 'Secarse', 'Temblar', 'Sudar'], respuesta: 0 },
+            { japones: '喘ぐ', lectura: 'aegu', opciones: ['Jadear', 'Reír', 'Llorar', 'Dormir'], respuesta: 0 },
+            { japones: '快感', lectura: 'kaikan', opciones: ['Sensación placentera', 'Dolor', 'Miedo', 'Aburrimiento'], respuesta: 0 },
+            { japones: '密着', lectura: 'micchaku', opciones: ['Pegarse estrechamente', 'Separarse', 'Empujar', 'Alejarse'], respuesta: 0 },
+            { japones: '感触', lectura: 'kanshoku', opciones: ['Sensación al tacto', 'Sonido', 'Sabor', 'Olor'], respuesta: 0 },
+            { japones: '甘える', lectura: 'amaeru', opciones: ['Actuar mimado', 'Enfadarse', 'Ignorar', 'Rechazar'], respuesta: 0 },
+            { japones: '疼く', lectura: 'uzuku', opciones: ['Latir/excitarse', 'Dormir', 'Comer', 'Correr'], respuesta: 0 },
+            { japones: 'もだえる', lectura: 'modaeru', opciones: ['Retorcerse de placer', 'Quedarse quieto', 'Saltar', 'Caer'], respuesta: 0 }
+        ],
+        'mazo2': [
+            { japones: '艶めく', lectura: 'tsuyameku', opciones: ['Volverse sensual', 'Ponerse serio', 'Enfadarse', 'Alegrarse'], respuesta: 0 },
+            { japones: '艶やか', lectura: 'tsuyayaka', opciones: ['Sensual/seductor', 'Feo', 'Común', 'Aburrido'], respuesta: 0 },
+            { japones: '淫ら', lectura: 'midara', opciones: ['Lascivo/obsceno', 'Inocente', 'Tímido', 'Serio'], respuesta: 0 },
+            { japones: '疼き', lectura: 'uzuki', opciones: ['Excitación/latido', 'Dolor', 'Cansancio', 'Sueño'], respuesta: 0 },
+            { japones: '恍惚', lectura: 'koukotsu', opciones: ['Éxtasis/trance', 'Tristeza', 'Enojo', 'Miedo'], respuesta: 0 },
+            { japones: '陶酔', lectura: 'tousui', opciones: ['Embriagarse de placer', 'Despertar', 'Dormir', 'Correr'], respuesta: 0 },
+            { japones: '熱情', lectura: 'netsujou', opciones: ['Pasión ardiente', 'Frío', 'Indiferencia', 'Odio'], respuesta: 0 },
+            { japones: '甘美', lectura: 'kanbi', opciones: ['Dulce/placentero', 'Amargo', 'Ácido', 'Salado'], respuesta: 0 },
+            { japones: '衝動', lectura: 'shoudou', lectura: 'shōdō', opciones: ['Impulso/deseo', 'Razón', 'Pensamiento', 'Duda'], respuesta: 0 },
+            { japones: '悦び', lectura: 'yorokobi', opciones: ['Deleite/placer', 'Tristeza', 'Dolor', 'Miedo'], respuesta: 0 }
+        ],
+        'mazo3': [
+            { japones: '焦がれる', lectura: 'kogareru', opciones: ['Anhelar/desear', 'Odiar', 'Temer', 'Ignorar'], respuesta: 0 },
+            { japones: '求める', lectura: 'motomeru', opciones: ['Buscar/exigir', 'Rechazar', 'Evitar', 'Olvidar'], respuesta: 0 },
+            { japones: '酔い痴れる', lectura: 'yoishireru', opciones: ['Embriagarse de pasión', 'Despertar', 'Dormir', 'Comer'], respuesta: 0 },
+            { japones: '蕩ける', lectura: 'torokeru', opciones: ['Derretirse de placer', 'Endurecerse', 'Secarse', 'Congelarse'], respuesta: 0 },
+            { japones: '悶える', lectura: 'modaeru', opciones: ['Gemir de placer/dolor', 'Reír', 'Cantar', 'Hablar'], respuesta: 0 },
+            { japones: 'たゆたう', lectura: 'tayutau', opciones: ['Ondular/balancearse', 'Quedarse quieto', 'Caer', 'Saltar'], respuesta: 0 },
+            { japones: 'もつれ合う', lectura: 'motsureau', opciones: ['Enredarse juntos', 'Separarse', 'Pelear', 'Ignorarse'], respuesta: 0 },
+            { japones: '絡み合う', lectura: 'karamiau', opciones: ['Entrelazarse', 'Desenredarse', 'Alejarse', 'Evitarse'], respuesta: 0 },
+            { japones: '貪る', lectura: 'musaboru', opciones: ['Devorar/codiciar', 'Rechazar', 'Compartir', 'Guardar'], respuesta: 0 },
+            { japones: '溺れる', lectura: 'oboreru', opciones: ['Ahogarse en placer', 'Flotar', 'Nadar', 'Salir'], respuesta: 0 }
+        ],
+        'mazo4': [
+            { japones: '官能的', lectura: 'kannouteki', opciones: ['Sensual/erótico', 'Aburrido', 'Serio', 'Infantil'], respuesta: 0 },
+            { japones: '妖艶', lectura: 'youen', opciones: ['Fascinante/seductor', 'Feo', 'Simple', 'Común'], respuesta: 0 },
+            { japones: '色香', lectura: 'iroka', opciones: ['Encanto/atractivo sexual', 'Fealdad', 'Neutralidad', 'Timidez'], respuesta: 0 },
+            { japones: '色気', lectura: 'iroke', opciones: ['Atractivo sexual', 'Indiferencia', 'Aversión', 'Miedo'], respuesta: 0 },
+            { japones: '色っぽい', lectura: 'iroppoi', opciones: ['Sexy/seductor', 'Poco atractivo', 'Neutro', 'Infantil'], respuesta: 0 },
+            { japones: '艶っぽい', lectura: 'tsuyappoi', opciones: ['Sensual/provocativo', 'Serio', 'Aburrido', 'Grosero'], respuesta: 0 },
+            { japones: '艶やか', lectura: 'tsuyayaka', opciones: ['Brillante/seductor', 'Opaco', 'Feo', 'Común'], respuesta: 0 },
+            { japones: '色めく', lectura: 'iromeku', opciones: ['Excitarse/animarse', 'Abrurrirse', 'Entristecerse', 'Dormirse'], respuesta: 0 },
+            { japones: '色めいた', lectura: 'iromoita', opciones: ['Coqueto/provocativo', 'Serio', 'Tímido', 'Enojado'], respuesta: 0 },
+            { japones: '妖しい', lectura: 'ayashii', opciones: ['Misterioso/seductor', 'Común', 'Abierto', 'Simple'], respuesta: 0 }
+        ],
+        'mazo5': [
+            { japones: '官能小説', lectura: 'kannoushousetsu', opciones: ['Novela erótica', 'Novela policiaca', 'Novela histórica', 'Novela infantil'], respuesta: 0 },
+            { japones: '官能描写', lectura: 'kannoubyousha', opciones: ['Descripción sensual', 'Descripción técnica', 'Descripción histórica', 'Descripción geográfica'], respuesta: 0 },
+            { japones: '艶めかしい', lectura: 'tsuyamekashii', opciones: ['Sensual/erótico', 'Inocente', 'Serio', 'Aburrido'], respuesta: 0 },
+            { japones: '色欲', lectura: 'shikiyoku', opciones: ['Deseo sexual/lujuria', 'Apetito', 'Sed', 'Sueño'], respuesta: 0 },
+            { japones: '性欲', lectura: 'seiyoku', opciones: ['Deseo sexual', 'Hambre', 'Sed', 'Cansancio'], respuesta: 0 },
+            { japones: '情欲', lectura: 'jouyoku', opciones: ['Pasión sexual', 'Odio', 'Amistad', 'Indiferencia'], respuesta: 0 },
+            { japones: '肉欲', lectura: 'nikuyoku', opciones: ['Deseo carnal', 'Espiritualidad', 'Intelecto', 'Emoción'], respuesta: 0 },
+            { japones: '愛欲', lectura: 'aiyoku', opciones: ['Deseo amoroso', 'Odio', 'Indiferencia', 'Miedo'], respuesta: 0 },
+            { japones: '肉感的', lectura: 'nikukanteki', opciones: ['Carnal/sensual', 'Espiritual', 'Intelectual', 'Emocional'], respuesta: 0 },
+            { japones: '肉体的', lectura: 'nikutaitaki', opciones: ['Físico/corporal', 'Mental', 'Espiritual', 'Emocional'], respuesta: 0 }
+        ]
+    },
+    
+    // Colección 2 - Tsundere Enojada
+    'coleccion2': {
+        'mazo1': [
+            { japones: '照れる', lectura: 'tereru', opciones: ['Avergonzarse', 'Enfadarse', 'Alegrarse', 'Asustarse'], respuesta: 0 },
+            { japones: '恥ずかしい', lectura: 'hazukashii', opciones: ['Vergonzoso', 'Valiente', 'Fácil', 'Difícil'], respuesta: 0 },
+            { japones: '赤面', lectura: 'sekimen', opciones: ['Ponerse rojo', 'Ponerse pálido', 'Ponerse verde', 'Ponerse azul'], respuesta: 0 },
+            { japones: '戸惑う', lectura: 'tomadou', opciones: ['Sentirse confundido', 'Sentirse seguro', 'Sentirse feliz', 'Sentirse triste'], respuesta: 0 },
+            { japones: '動揺', lectura: 'douyou', opciones: ['Agitación/perturbación', 'Calma', 'Paz', 'Tranquilidad'], respuesta: 0 },
+            { japones: '焦る', lectura: 'aseru', opciones: ['Apressurarse', 'Relajarse', 'Dormir', 'Esperar'], respuesta: 0 },
+            { japones: '誤魔化す', lectura: 'gomakasu', opciones: ['Engañar/ocultar', 'Confesar', 'Mostrar', 'Explicar'], respuesta: 0 },
+            { japones: '拗ねる', lectura: 'suneru', opciones: ['Hacerse el desentendido', 'Cooperar', 'Ayudar', 'Agradecer'], respuesta: 0 },
+            { japones: '素直', lectura: 'sunao', opciones: ['Honesto/sincero', 'Mentiroso', 'Falso', 'Hipócrita'], respuesta: 0 },
+            { japones: 'ぶつかる', lectura: 'butsukaru', opciones: ['Chocar/toparse', 'Esquivar', 'Evitar', 'Ignorar'], respuesta: 0 }
+        ],
+        'mazo2': [
+            { japones: 'ぶっきらぼう', lectura: 'bukkirabou', opciones: ['Grosero/directo', 'Amable', 'Educado', 'Cortés'], respuesta: 0 },
+            { japones: 'つれない', lectura: 'tsurenai', opciones: ['Frío/indiferente', 'Cálido', 'Cariñoso', 'Amable'], respuesta: 0 },
+            { japones: '素っ気ない', lectura: 'sokkenai', opciones: ['Frío/descortés', 'Caluroso', 'Acogedor', 'Amistoso'], respuesta: 0 },
+            { japones: '突っ慳貪', lectura: 'tsukkendon', opciones: ['Directo/sin rodeos', 'Indirecto', 'Sutil', 'Discreto'], respuesta: 0 },
+            { japones: '四角四面', lectura: 'shikakushimen', opciones: ['Formal/rígido', 'Informal', 'Flexible', 'Relajado'], respuesta: 0 },
+            { japones: '意地悪', lectura: 'ijiwaru', opciones: ['Malvado/mezquino', 'Bondadoso', 'Amable', 'Generoso'], respuesta: 0 },
+            { japones: '辛辣', lectura: 'shinratsu', opciones: ['Mordaz/cínico', 'Suave', 'Amable', 'Dulce'], respuesta: 0 },
+            { japones: '毒舌', lectura: 'dokuzetsu', opciones: ['Lengua viperina', 'Elogioso', 'Amable', 'Cortés'], respuesta: 0 },
+            { japones: '皮肉', lectura: 'hiniku', opciones: ['Irónico/sarcástico', 'Sincero', 'Directo', 'Honesto'], respuesta: 0 },
+            { japones: '嫌味', lectura: 'iyami', opciones: ['Desagradable/hostil', 'Agradable', 'Amable', 'Encantador'], respuesta: 0 }
+        ],
+        // Más mazos para colección 2...
+        'mazo3': [
+            { japones: '突っ張る', lectura: 'tsupparu', opciones: ['Ponerse firme', 'Ceder', 'Rendirse', 'Retirarse'], respuesta: 0 },
+            { japones: '意地っ張り', lectura: 'ijippari', opciones: ['Testarudo/terco', 'Flexible', 'Complacidor', 'Sumiso'], respuesta: 0 },
+            { japones: '強がる', lectura: 'tsuyogaru', opciones: ['Fingir fortaleza', 'Mostrar debilidad', 'Pedir ayuda', 'Llorar'], respuesta: 0 },
+            { japones: '見栄っ張り', lectura: 'mieppari', opciones: ['Presumido/vanidoso', 'Humilde', 'Modesto', 'Sencillo'], respuesta: 0 },
+            { japones: '背伸び', lectura: 'senobi', opciones: ['Intentar aparentar más', 'Ser uno mismo', 'Rebajarse', 'Minimizarse'], respuesta: 0 },
+            { japones: '取り澄ます', lectura: 'torisumasu', opciones: ['Ponerse serio/formal', 'Relajarse', 'Bromear', 'Jugar'], respuesta: 0 },
+            { japones: '気取る', lectura: 'kidorur', opciones: ['Aparentar/dar importancia', 'Ser natural', 'Ser espontáneo', 'Ser auténtico'], respuesta: 0 },
+            { japones: '知ったかぶり', lectura: 'shittakaburi', opciones: ['Fingir saber', 'Reconocer ignorancia', 'Preguntar', 'Aprender'], respuesta: 0 },
+            { japones: '勿体ぶる', lectura: 'mottai buru', opciones: ['Darse importancia', 'Ser humilde', 'Ser modesto', 'Ser sencillo'], respuesta: 0 },
+            { japones: '小賢しい', lectura: 'kozakashii', opciones: ['Astuto/arrogante', 'Honesto', 'Sincero', 'Ingenuo'], respuesta: 0 }
+        ],
+        'mazo4': [
+            { japones: '拗らせる', lectura: 'nejiraseru', opciones: ['Complicar/retorcer', 'Simplificar', 'Facilitar', 'Aclarar'], respuesta: 0 },
+            { japones: '捻くれる', lectura: 'nejikureru', opciones: ['Torcerse/volverse cínico', 'Enderezarse', 'Aclararse', 'Simplificarse'], respuesta: 0 },
+            { japones: 'ひねくれる', lectura: 'hinekureru', opciones: ['Volverse retorcido', 'Volverse recto', 'Volverse simple', 'Volverse claro'], respuesta: 0 },
+            { japones: 'すねる', lectura: 'suneru', opciones: ['Enfadarse/ponerse quisquilloso', 'Coopera', 'Agradecer', 'Ayudar'], respuesta: 0 },
+            { japones: 'ぐずる', lectura: 'guzuru', opciones: ['Quejarse/refunfuñar', 'Aceptar', 'Agradecer', 'Cooperar'], respuesta: 0 },
+            { japones: 'ふくれる', lectura: 'fukureru', opciones: ['Hincharse/enfadarse', 'Deshincharse', 'Alegrarse', 'Relajarse'], respuesta: 0 },
+            { japones: 'むくれる', lectura: 'mukureru', opciones: ['Enfadarse/ponerse hosco', 'Alegrarse', 'Sonreír', 'Abrazar'], respuesta: 0 },
+            { japones: 'つぷす', lectura: 'tsupusu', opciones: ['Golpear (suavemente)', 'Acariciar', 'Besar', 'Abrazar'], respuesta: 0 },
+            { japones: 'ぽんぽん', lectura: 'ponpon', opciones: ['Golpear suavemente', 'Acariciar', 'Besar', 'Lamer'], respuesta: 0 },
+            { japones: 'こつん', lectura: 'kotsun', opciones: ['Golpe ligero', 'Abrazo', 'Beso', 'Caricia'], respuesta: 0 }
+        ],
+        'mazo5': [
+            { japones: '照れ屋', lectura: 'tereya', opciones: ['Persona tímida', 'Persona extrovertida', 'Persona valiente', 'Persona audaz'], respuesta: 0 },
+            { japones: '恥ずかしがり屋', lectura: 'hazukashigariya', opciones: ['Persona vergonzosa', 'Persona desvergonzada', 'Persona confiada', 'Persona atrevida'], respuesta: 0 },
+            { japones: '人見知り', lectura: 'hitomishiri', opciones: ['Timidez con desconocidos', 'Sociabilidad', 'Extroversión', 'Confianza'], respuesta: 0 },
+            { japones: '内気', lectura: 'uchiki', opciones: ['Tímido/introvertido', 'Extrovertido', 'Sociable', 'Comunicativo'], respuesta: 0 },
+            { japones: '奥手', lectura: 'okute', opciones: ['Tímido en el amor', 'Atrevido en el amor', 'Despreocupado', 'Indiferente'], respuesta: 0 },
+            { japones: '陰性', lectura: 'insei', opciones: ['De carácter reservado', 'De carácter abierto', 'Extrovertido', 'Sociable'], respuesta: 0 },
+            { japones: '引っ込み思案', lectura: 'hikkomijian', opciones: ['Tímido/retraído', 'Arriesgado', 'Audaz', 'Atrevido'], respuesta: 0 },
+            { japones: '恥ずかしそう', lectura: 'hazukashisou', opciones: ['Parecer avergonzado', 'Parecer confiado', 'Parecer feliz', 'Parecer triste'], respuesta: 0 },
+            { japones: 'もじもじ', lectura: 'mojimoji', opciones: ['Comportarse tímidamente', 'Comportarse con confianza', 'Comportarse con audacia', 'Comportarse con arrogancia'], respuesta: 0 },
+            { japones: 'てれてれ', lectura: 'teretere', opciones: ['Actuar tímidamente', 'Actuar con confianza', 'Actuar atrevidamente', 'Actuar arrogantemente'], respuesta: 0 }
+        ]
+    },
+    
+    // Colecciones 3, 4 y 5 tendrán estructura similar...
+    'coleccion3': {
+        'mazo1': [
+            { japones: '恥じらう', lectura: 'hajirau', opciones: ['Actuar tímidamente', 'Actuar con confianza', 'Actuar arrogantemente', 'Actuar indiferentemente'], respuesta: 0 },
+            { japones: '俯く', lectura: 'utsumuku', opciones: ['Bajar la vista', 'Mirar fijamente', 'Mirar hacia arriba', 'Mirar de reojo'], respuesta: 0 },
+            { japones: 'もじもじ', lectura: 'mojimoji', opciones: ['Moverse nerviosamente', 'Quedarse quieto', 'Moverse con confianza', 'Moverse rápidamente'], respuesta: 0 },
+            { japones: 'てれる', lectura: 'tereru', opciones: ['Avergonzarse', 'Enfadarse', 'Alegrarse', 'Entristecerse'], respuesta: 0 },
+            { japones: '照れる', lectura: 'tereru', opciones: ['Ponerse colorado', 'Ponerse pálido', 'Ponerse serio', 'Ponerse alegre'], respuesta: 0 },
+            { japones: '赤らむ', lectura: 'akaramu', opciones: ['Ponerse rojo', 'Ponerse blanco', 'Ponerse verde', 'Ponerse azul'], respuesta: 0 },
+            { japones: 'はにかむ', lectura: 'hanikamu', opciones: ['Sonreír tímidamente', 'Reír a carcajadas', 'Llorar', 'Gritar'], respuesta: 0 },
+            { japones: 'きまり悪い', lectura: 'kimariwarui', opciones: ['Sentirse incómodo', 'Sentirse cómodo', 'Sentirse feliz', 'Sentirse enojado'], respuesta: 0 },
+            { japones: '気恥ずかしい', lectura: 'kihazukashii', opciones: ['Sentirse avergonzado', 'Sentirse orgulloso', 'Sentirse valiente', 'Sentirse seguro'], respuesta: 0 },
+            { japones: 'きゅんとする', lectura: 'kyuntosuru', opciones: ['Sentir un latido emocional', 'Sentir indiferencia', 'Sentir enojo', 'Sentir miedo'], respuesta: 0 }
+        ],
+        // ... más mazos para colección 3
+    },
+    
+    'coleccion4': {
+        'mazo1': [
+            { japones: '躍動', lectura: 'yakudou', opciones: ['Movimiento dinámico', 'Inmovilidad', 'Calma', 'Paz'], respuesta: 0 },
+            { japones: '活発', lectura: 'kappatsu', opciones: ['Activo/energético', 'Pasivo', 'Perzoso', 'Inactivo'], respuesta: 0 },
+            { japones: '元気', lectura: 'genki', opciones: ['Energía/vitalidad', 'Cansancio', 'Debilidad', 'Enfermedad'], respuesta: 0 },
+            { japones: '溌剌', lectura: 'hatsuratsu', opciones: ['Fresco/vigoroso', 'Cansado', 'Agotado', 'Débil'], respuesta: 0 },
+            { japones: '爽やか', lectura: 'sawayaka', opciones: ['Refrescante', 'Pesado', 'Cargado', 'Opresivo'], respuesta: 0 },
+            { japones: '快活', lectura: 'kaikatsu', opciones: ['Alegre/animado', 'Triste', 'Deprimido', 'Melancólico'], respuesta: 0 },
+            { japones: '溌溂', lectura: 'hatsuratsu', opciones: ['Lleno de vida', 'Sin vida', 'Apático', 'Indiferente'], respuesta: 0 },
+            { japones: '弾む', lectura: 'hazumu', opciones: ['Rebotar/estar animado', 'Caer', 'Quedarse quieto', 'Disminuir'], respuesta: 0 },
+            { japones: '躍る', lectura: 'odoru', opciones: ['Saltar/bailar', 'Quedarse quieto', 'Sentarse', 'Acostarse'], respuesta: 0 },
+            { japones: '跳ねる', lectura: 'haneru', opciones: ['Saltar/brincar', 'Caer', 'Rodar', 'Deslizarse'], respuesta: 0 }
+        ],
+        // ... más mazos para colección 4
+    },
+    
+    'coleccion5': {
+        'mazo1': [
+            { japones: '食欲', lectura: 'shokuyoku', opciones: ['Apetito', 'Saciedad', 'Náusea', 'Indigestión'], respuesta: 0 },
+            { japones: '食いしん坊', lectura: 'kuishinbou', opciones: ['Glotón', 'Inapetente', 'Dietista', 'Vegetariano'], respuesta: 0 },
+            { japones: '大食い', lectura: 'oogui', opciones: ['Comilón', 'Poco comedor', 'Moderado', 'Abstemio'], respuesta: 0 },
+            { japones: '健啖', lectura: 'kentan', opciones: ['Tener buen apetito', 'Tener mal apetito', 'Ser delicado', 'Ser exigente'], respuesta: 0 },
+            { japones: '食い意地', lectura: 'kuiiji', opciones: ['Gula/egoísmo con comida', 'Generosidad', 'Compartir', 'Ayunar'], respuesta: 0 },
+            { japones: '食い気', lectura: 'kuiki', opciones: ['Deseo de comer', 'Falta de apetito', 'Asco', 'Náusea'], respuesta: 0 },
+            { japones: '旺盛', lectura: 'ousei', opciones: ['Abundante/vigoroso', 'Escaso', 'Débil', 'Limitado'], respuesta: 0 },
+            { japones: '旺盛な食欲', lectura: 'ousei na shokuyoku', opciones: ['Apetito voraz', 'Poco apetito', 'Apetito normal', 'Sin apetito'], respuesta: 0 },
+            { japones: '貪る', lectura: 'musaboru', opciones: ['Devorar', 'Comer lentamente', 'Probar', 'Dejar'], respuesta: 0 },
+            { japones: 'むしゃむしゃ', lectura: 'mushamusha', opciones: ['Comer ruidosamente', 'Comer en silencio
